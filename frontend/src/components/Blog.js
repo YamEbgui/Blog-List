919d31badbeb0bf35815a9bfb7d0c1f225d6d6b5
Yam Ebgui <yam.ebgui@gmail.com>
2021-12-17 21:39:16 +0200

    Update for blog: if the user clicks on the 'view' button he can see all the details that the blog contains

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,8 +1,55 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const [fullyDisplayed, setDisplay] = useState(false);
 
-export default Blog
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const buttonStyle = {
+    display: "block",
+    "margin-left": "auto",
+    "margin-right": 0,
+  };
+
+  const toggleDisplayBlog = () => {
+    setDisplay(!fullyDisplayed);
+  };
+
+  const fullDisplay = () => {
+    return (
+      <div style={blogStyle}>
+        <button style={buttonStyle} onClick={toggleDisplayBlog}>
+          hide
+        </button>
+        <p>{blog.title}</p>
+        <p>{blog.author}</p>
+        <a href={blog.url}>
+          <p>{blog.url}</p>
+        </a>
+        <p>
+          {"likes: " + blog.likes} <button>👍</button>
+        </p>
+        <p>{blog.user.name}</p>
+      </div>
+    );
+  };
+
+  const shortDisplay = () => {
+    return (
+      <div style={blogStyle}>
+        <button style={buttonStyle} onClick={toggleDisplayBlog}>
+          view
+        </button>
+        {blog.title} {blog.author}{" "}
+      </div>
+    );
+  };
+
+  return <div>{fullyDisplayed === true ? fullDisplay() : shortDisplay()}</div>;
+};
+export default Blog;
